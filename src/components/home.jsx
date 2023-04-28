8332757bcf3a7c38c734d35e344faad66e4a40ff
Fabricio <fabriciogodoy95@hotmail.com>
2023-04-28 18:16:57 -0300

    estilos

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -4,7 +4,13 @@ import fotoCV from '../../public/img/fotofabricv.jpg'
 export const Home = () => {
   return (
   <div className='homePage'> 
-   <h1 className=''>Home</h1>
+  
+   <div className='containerTitlle'>
+     <h3 className='titlleh3'>Hola, soy</h3>
+     <h1 className='titlleh1'>Fabricio Godoy</h1>
+     <h3 className='titlleh5'>Programador Web Full Stack</h3>
+   </div>
+
     <img className='imgHome' src= {fotoCV}/>
   </div>
   )
