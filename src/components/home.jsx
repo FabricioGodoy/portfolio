f9ce497e2d2374eadbef03ba2c32107618722208
Fabricio <fabriciogodoy95@hotmail.com>
2023-05-02 18:26:02 -0300

    scroll animated

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,17 +1,35 @@
 import './style.css'
-import fotoCV from '../../public/img/fotofabricv.jpg'
+/* import fotoCV from '../../public/img/fotofabricv.jpg' */
+import error404 from '../../public/img/404-error.jpg'
+import iconJS from '../../public/img/icons8-javascript-48.png'
+import reactJs from '../../public/img/reactLogo.png'
+import nodeJs from '../../public/img/logoNode.png'
 
 export const Home = () => {
-  return (
-  <div className='homePage'> 
   
-   <div className='containerTitlle'>
-     <h3 className='titlleh3'>Hola, soy</h3>
-     <h1 className='titlleh1'>Fabricio Godoy</h1>
-     <h3 className='titlleh5'>Programador Web Full Stack</h3>
-   </div>
+  return (
+  <>
+      <div className='homePage'> 
+   
+        <div className='containerTitlle'>
+          <h3 className='titlleh3'>Hola, soy</h3>
+          <h1 className='titlleh1'>Fabricio Godoy</h1>
+          <h3 className='titlleh5'>Programador Web Full Stack</h3>
+        </div>
+    
+        <div className='containerImgLogos'>
+          
+            <img className='imgHome' src= {error404}/>
+          
+          <div className='containerLogos'>
+            <img className='logoIcon' src= {iconJS}/>
+            <img className='logoIcon' src= {reactJs}/>
+            <img className='logoIcon' src= {nodeJs}/>
+          </div>
 
-    <img className='imgHome' src= {fotoCV}/>
-  </div>
+        </div>
+
+      </div>
+  </>
   )
 }
